b0e07068ff88bbf64784fc407fca329bd29a9672
anji hi <anji.hi@gmail.com>
2018-07-18 23:54:36 +0900

    ハッシュタグの検索ができない問題を修正。

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 chrome.omnibox.onInputEntered.addListener(
     function(text){
+	text = text.replace("#", "%23");
 	var url = "http://realtime.search.yahoo.co.jp/search?p=" + text;
         chrome.tabs.getSelected(null, function(tab) {
             chrome.tabs.update(tab.id, {url:url});
